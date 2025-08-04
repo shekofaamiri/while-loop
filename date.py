10bd66f9a2f18e55552ef16bb4c7fdd4f5f472bb
shekofaamiri <shekofaamiri1@gmail.com>
2025-08-04 23:01:06 +0330

    setter and getter

--- date.py
+++ date.py
@@ -12,3 +12,24 @@ class Date{
     int get_year();
     void print_date();
 };
+void Date::set_day(int d){
+    day=d;
+}
+int Date::get_day(){
+    return day;
+}
+void Date::set_month(int m){
+    month=m;
+}
+int Date::get_month(){
+    return month;
+}
+void Date::set_year(int y){
+    year=y;
+}
+int Date::get_year(){
+    return year;
+}
+void Date::print_date(){
+    cout<<year<<"/"<<month<<"/"<<day;
+}
